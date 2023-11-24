b6036a9f9873c8f5fbe26c9cd67a7a57561e4cdf
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2023-11-24 23:03:43 +0400

    finial

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
-const inputvalue=document.getElementById('inputval');
+const inputvalue=document.querySelector(".inputval")
 const year=parseInt(inputvalue.value);
-const isLeap=()=>{
-    return ((year % 4 == 0 && year % 100 !=0 ) || year % 400 == 0);
+const isLeap=(year)=>{
+    console.log(parseInt(inputvalue.value , 10));
+    return ((year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0);
 };
-    const btn = document
-    .getElementById("btn")
-    .addEventListener("click", function () {
-        document.getElementById("output").textContent =
-            isLeap(year.value);
-        console.log(isLeap(year.value));
-    });
+const btn = document
+            .getElementById("btn")
+            .addEventListener("click", function () {
+                document.getElementById("output").textContent =
+                    isLeap(parseInt(inputvalue.value , 10));
+            });
